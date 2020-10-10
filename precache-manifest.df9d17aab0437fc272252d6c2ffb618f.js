1208d880acd812139bb19124915baa1c25720e9a
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2020-10-10 11:50:23 +0530

    Update precache-manifest.df9d17aab0437fc272252d6c2ffb618f.js

--- precache-manifest.df9d17aab0437fc272252d6c2ffb618f.js
+++ precache-manifest.df9d17aab0437fc272252d6c2ffb618f.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "001ec4113bb0c6515b428a5e291368b8",
-    "url": "/index.html"
+    "url": "/pokemon_react/index.html"
   },
   {
     "revision": "c40484e1b0045cbd3e5b",
-    "url": "/static/css/main.3ac02b9a.chunk.css"
+    "url": "/pokemon_react/static/css/main.3ac02b9a.chunk.css"
   },
   {
     "revision": "9b5a5336800f47f88377",
-    "url": "/static/js/2.14b2fb4c.chunk.js"
+    "url": "/pokemon_react/static/js/2.14b2fb4c.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/static/js/2.14b2fb4c.chunk.js.LICENSE.txt"
+    "url": "/pokemon_react/static/js/2.14b2fb4c.chunk.js.LICENSE.txt"
   },
   {
     "revision": "c40484e1b0045cbd3e5b",
-    "url": "/static/js/main.aab021bd.chunk.js"
+    "url": "/pokemon_react/static/js/main.aab021bd.chunk.js"
   },
   {
     "revision": "965aca300182a94c14f3",
-    "url": "/static/js/runtime-main.4126d0af.js"
+    "url": "/pokemon_react/static/js/runtime-main.4126d0af.js"
   }
 ]);
